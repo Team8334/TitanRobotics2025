a2145f32969b1fa9c2f322e4da6e9917ad985490
BismuthTime <122326994+echo2334@users.noreply.github.com>
2024-10-24 16:23:36 -0600

    Update ExampleSubsystem.java
    Wrote an example subsystem to show how they work. Included comments explaining each individual function.

--- RobotCode2025/src/main/java/frc/robot/Subsystems/ExampleSubsystem.java
+++ RobotCode2025/src/main/java/frc/robot/Subsystems/ExampleSubsystem.java
@@ -3,7 +3,11 @@ package frc.robot.Subsystems;
 public class ExampleSubsystem implements Subsystem{ 
     private static ExampleSubsystem instance = null;
 
-    public static ExampleSubsystem getInstance(){
+    private int exampleVariable;
+    private double exampleVariable2;
+    private boolean running;
+
+    public static ExampleSubsystem getInstance(){ //Returns the current instance of the class.
         if(instance==null)
         {
             instance = new ExampleSubsystem();
@@ -11,9 +15,51 @@ public class ExampleSubsystem implements Subsystem{
             return instance;
     }
  
-    public ExampleSubsystem(){
+    public ExampleSubsystem(){ //Constructor
 
     }
 
+    public double getExampleVariable2(){ //Filler Function
+        return exampleVariable2;
+    }
+
+    public void exampleVariableState(){ //Filler Function
+        switch(exampleVariable){
+            case 1:
+                System.out.println("ExampleVariable is 1");
+            break;
+            case 2:
+                System.out.println("ExampleVariable is 2");
+            break;
+            case 3:
+                System.out.println("ExampleVariable is 3");
+            break;
+            default:
+                System.out.println("ExampleVariable is less than 1 or greater than 3");
+        }
+    }
+
+    public boolean isActive(){ //Returns if the subsystem is actively running. 
+        if(running){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public String getName(){ //Returns the name of the subsystem.
+        return "ExampleSubsystem";
+    }
+
+    public void initialize(){ //Initializes the variables of a class, like motors for example. Place the definitions of your variables here.
+        exampleVariable = 4;
+        exampleVariable2 = 3.5;
+        running = false;
+    }
+
+    public void update(){ //Used to update specific functions that need to be ran continuously. For example, running a camera.
+        
+    }
     
 }
