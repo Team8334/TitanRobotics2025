bc4c5d7d945be570eeb76b7d43fcc8b369ec1bc2
BismuthTime <122326994+echo2334@users.noreply.github.com>
2024-11-19 16:28:16 -0700

    i think i made things work, idk yet, just committing
    making the device for the gyro

--- RobotCode2025/src/main/java/frc/robot/Devices/NavxIMU.java
+++ RobotCode2025/src/main/java/frc/robot/Devices/NavxIMU.java
@@ -1,11 +1,32 @@
 package frc.robot.Devices;
 
 import frc.robot.Interfaces.Devices;
+import frc.robot.Interfaces.Gyro;
 
-public class NavxIMU implements Devices {
+public class NavxIMU implements Devices, Gyro {
 
      public boolean isEnabled() {
         return this.isEnabled();
     }
 
+    @Override
+    public double getyaw() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    @Override 
+    public double getRate() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    @Override 
+    public double getCompassHeading() {
+        // TODO Auto-generated method stub  
+        return 0;
+    }
+    @Override 
+    public double getRawAccelZ() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 }
