cc8fada5f6ed9d8a868f32558384bb9e40745699
BismuthTime <122326994+echo2334@users.noreply.github.com>
2024-11-21 16:52:44 -0700

    added accelerometer and continued work on navx device

--- RobotCode2025/src/main/java/frc/robot/Devices/NavxIMU.java
+++ RobotCode2025/src/main/java/frc/robot/Devices/NavxIMU.java
@@ -2,11 +2,22 @@ package frc.robot.Devices;
 
 import frc.robot.Interfaces.Devices;
 import frc.robot.Interfaces.Gyro;
+import frc.robot.Interfaces.accelerometer;
+import com.kauailabs.navx.frc.AHRS;
 
-public class NavxIMU implements Devices, Gyro {
+import edu.wpi.first.wpilibj.SPI;
+
+public class NavxIMU implements Devices, Gyro, accelerometer {
 
      public boolean isEnabled() {
         return this.isEnabled();
+    }
+
+    AHRS ahrs;
+
+    public NavxIMU()
+    {
+         ahrs = new AHRS(SPI.Port.kMXP);
     }
 
     @Override
@@ -28,5 +39,47 @@ public class NavxIMU implements Devices, Gyro {
     public double getRawAccelZ() {
         // TODO Auto-generated method stub
         return 0;
+    }
+
+    @Override
+    public void resetDisplacement() {
+        // TODO Auto-generated method stub
+        public void resetDisplacement() {};
+    }
+
+    @Override
+    public float getDisplacementX() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDisplacementX'");
+    }
+
+    @Override
+    public float getDisplacementY() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDisplacementY'");
+    }
+
+    @Override
+    public float getDisplacementZ() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDisplacementZ'");
+    }
+
+    @Override
+    public double getVelocityX() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getVelocityX'");
+    }
+
+    @Override
+    public double getVelocityY() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getVelocityY'");
+    }
+
+    @Override
+    public double getVelocityZ() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getVelocityZ'");
     }
 }
---
+++ RobotCode2025/src/main/java/frc/robot/Interfaces/accelerometer.java
@@ -0,0 +1,12 @@
+package frc.robot.Interfaces;
+
+public interface accelerometer {
+    public void resetDisplacement();
+    public float getDisplacementX();
+    public float getDisplacementY();
+    public float getDisplacementZ();
+    public double getVelocityX();
+    public double getVelocityY();
+    public double getVelocityZ();
+
+}
